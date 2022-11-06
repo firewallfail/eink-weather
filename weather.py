92bd865df378de83a2b7de2eac6afa4fa6354014
Brent Hall <firewall.fail@hotmail.com>
2022-11-06 11:31:46 -0500

    restart

--- weather.py
+++ weather.py
@@ -4,30 +4,3 @@ from tkinter import *
 from tkinter import ttk
 
 
-def update(*args):
-    try:
-        current_weather.set("current weather")
-    except ValueError:
-        pass
-
-
-root = Tk()
-root.title("Weather")
-
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
-
-button = ttk.Button(root, text="Get Weather", command="update")
-button.grid()
-
-
-current_weather = StringVar()
-current_weather_label = ttk.Label(root, textvariable=current_weather)
-current_weather_label.grid()
-
-root.bind("<Return>", update)
-
-root.geometry("800x480")
-# root.overrideredirect(True)
-
-root.mainloop()
