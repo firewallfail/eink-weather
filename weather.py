33428ac263eda81f88ab77a7991e705f5bd67858
Brent Hall <firewall.fail@hotmail.com>
2022-11-27 14:56:48 -0500

    convert time to local

--- weather.py
+++ weather.py
@@ -1,25 +1,42 @@
 import requests
 import json
 import time
-import datetime
 
 from tkinter import *
 from tkinter import ttk
+from dateutil import tz
+from datetime import datetime, timedelta
 
 
+def time_to_local(time):
+    from_zone = tz.tzutc()
+    to_zone = tz.tzlocal()
+
+    utc = datetime.strptime(time, '%I:%M:%S %p')
+    utc = utc.replace(tzinfo=from_zone)
+    local = utc.astimezone(to_zone)
+
+    return local
 
 
 def get_sun_stats():
     lat = 42.984924
     lng = -81.245277
-    sun_api = f'https://api.sunrise-sunset.org/json?lat={lat}&lng={lng}'
-    r = requests.get(sun_api).json()
+    # temp hardcode time to stop hitting api needlessly
+    # sun_api = f'https://api.sunrise-sunset.org/json?lat={lat}&lng={lng}'
+    # r = requests.get(sun_api).json()
+    r = {
+        'results': {
+            'sunrise': '10:00:00 AM',
+            'sunset': '11:00:00 PM'
+        }
+    }
     results = r['results']
-    sun_rise = results['sunrise']
-    sun_set = results['sunset']
+    sun_rise = time_to_local(results['sunrise'])
+    sun_set = time_to_local(results['sunset'])
 
-    current_time = datetime.datetime.now()
-    midnight = (current_time + datetime.timedelta(days=1)).replace(hour=0, minute=0)
+    current_time = datetime.now()
+    midnight = (current_time + timedelta(days=1)).replace(hour=0, minute=0)
     next_update = ((midnight - current_time).seconds) * 1000 # ms until next update after midnight
 
     window.after(next_update, get_sun_stats) # once per day
@@ -56,16 +73,11 @@ unknown = ttk.Label(window, text="Undecided")
 unknown.grid(row=1, column=1, sticky=N)
 
 
-sun_stats = ttk.Label(window, text="Rise and set")
-sun_stats.rowconfigure(0, weight=1)
-sun_stats.rowconfigure(1, weight=1)
-sun_stats.columnconfigure(0, weight=1)
-sun_stats.columnconfigure(1, weight=1)
-sun_stats.columnconfigure(2, weight=1)
-sun_stats.grid(row=1, column=2, sticky=N)
+sun_stats = ttk.Frame(window)
+sun_stats.grid(row=1, column=2)
 
 sunrise = ttk.Label(sun_stats, text=sun_set)
-sunrise.grid(row=1, column=1, sticky=N)
+sunrise.grid(row=0, column=1, sticky=N)
 sunset = ttk.Label(sun_stats, text=sun_set)
 sunrise.grid(row=1, column=1, sticky=N)
 
