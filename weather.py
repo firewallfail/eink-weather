8c7710852ab820cb7ad2bd007bbef1a8373a7284
Brent Hall <firewall.fail@hotmail.com>
2022-11-27 00:38:58 -0500

    sun stuff

--- weather.py
+++ weather.py
@@ -1,7 +1,27 @@
 import requests
+import json
+import time
 
 from tkinter import *
 from tkinter import ttk
+
+
+
+
+def get_sun_stats():
+    lat = 42.984924
+    lng = -81.245277
+    sun_api = f'https://api.sunrise-sunset.org/json?lat={lat}&lng={lng}'
+    r = requests.get(sun_api).json()
+    results = r['results']
+    sun_rise = results['sunrise']
+    sun_set = results['sunset']
+
+    return
+# sleep for 3600 in main loop
+# if time between 00:00 and 2:00 grab sun_api
+# every hour update weather
+# time.sleep(3600)
 
 window = Tk()
 window.rowconfigure(0, weight=1)
@@ -26,7 +46,7 @@ date.grid(row=1, column=0, sticky=N)
 unknown = ttk.Label(window, text="Undecided")
 unknown.grid(row=1, column=1, sticky=N)
 
-sunrise = ttk.Label(window, text="Sunrise/set/lunar")
+sunrise = ttk.Label(window, text=sun_rise)
 sunrise.grid(row=1, column=2, sticky=N)
 
 window.geometry("800x480")
