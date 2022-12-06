318c500a600f4059c732da491bb134f192178689
Brent Hall <firewall.fail@hotmail.com>
2022-12-06 18:07:40 -0500

    lat/long fit open-meto requirements

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
 WEEKDAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-LAT = 42.984924
-LNG = -81.245277
+LAT = 42.9849
+LNG = -81.2453
