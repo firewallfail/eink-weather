00985daabfde77240e044465629ba759dccd94f9
Brent Hall <firewall.fail@hotmail.com>
2022-12-06 18:23:34 -0500

    weather codes map

--- constants.py
+++ constants.py
@@ -2,3 +2,31 @@ WEEKDAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 
 MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 LAT = 42.9849
 LNG = -81.2453
+
+WEATHER_CODE = {
+    0: 'Clear Sky',
+    1: 'Mainly Clear',
+    2: 'Partly Cloudy',
+    3: 'Overcase',
+    45: 'Fog',
+    48: 'Depositing Rime Fog',
+    51: 'Light Drizzle',
+    53: 'Moderate Drizzle',
+    55: 'Dense Drizzle',
+    56: 'Light Freezing Drizzle',
+    57: 'Dense Freezing Drizzle',
+    61: 'Slight Rain',
+    63: 'Moderate Rain',
+    65: 'Heavy Rain',
+    66: 'Light Freezing Rain',
+    67: 'Heavy Freezing Rain',
+    71: 'Slight Snow Fall',
+    73: 'Moderate Snow Fall',
+    75: 'Heavy Snow Fall',
+    77: 'Snow Grains',
+    80: 'Slight Rain Showers',
+    81: 'Moderate Rain Showers',
+    82: 'Heavy Rain Showers',
+    85: 'Slight Snow Showers',
+    86: 'Heavy Snow Showers'
+}
